f3c6b97553f6dd9328019a56bb0ef4ac149ff115
Jonas Minor <github@nc.seltsam.net>
2025-05-16 16:17:54 +0200

    Change python formatter to black

--- init.lua
+++ init.lua
@@ -468,8 +468,7 @@ require('lazy').setup({
             -- for you, so that they are available from within Neovim.
             local ensure_installed = vim.tbl_keys(servers or {})
             vim.list_extend(ensure_installed, {
-                -- 'black', -- Python formatter
-                'mypy', -- static type checker
+                'black', -- Python formatter
                 'ruff', -- Python linter
                 'stylua', -- Used to format Lua code
             })
