7dece97c9536566b2a63187a9ad1a66805572528
Jonas Minor <github@nc.seltsam.net>
2025-06-29 22:12:04 +0200

    Update and Fix lspconfig for mason-lspconfig changes

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -153,15 +153,9 @@ return {
                 },
             }
 
-            -- LSP servers and clients are able to communicate to each other what features they support.
-            --  By default, Neovim doesn't support everything that is in the LSP specification.
-            --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-            --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
-            local capabilities = require('blink.cmp').get_lsp_capabilities()
-
             -- Enable the following language servers
             --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-            --
+
             --  Add any additional override configuration in the following tables. Available keys are:
             --  - cmd (table): Override the default command used to start the server
             --  - filetypes (table): Override the default list of associated filetypes for the server
@@ -170,11 +164,6 @@ return {
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
                 -- clangd = {},
-                -- black = {
-                --     settings = {
-                --         linelength = 79,
-                --     },
-                -- },
                 -- gopls = {},
                 -- pyright = {},
                 -- rust_analyzer = {},
@@ -185,6 +174,17 @@ return {
                 --
                 -- But for many setups, the LSP (`ts_ls`) will work just fine
                 -- ts_ls = {},
+
+                -- Shellscript/Bash/Zsh
+                bashls = {
+                    filetypes = { 'sh', 'bash', 'zsh' },
+                },
+                shellcheck = {
+                    filetypes = { 'sh', 'bash', 'zsh' },
+                },
+                shfmt = {
+                    filetypes = { 'sh', 'bash', 'zsh' },
+                },
 
                 -- Latex / Markdown
                 ltex = {},
@@ -222,7 +222,11 @@ return {
                 },
                 stylua = {},
             }
-
+            ---@type MasonLspconfigSettings
+            ---@diagnostic disable-next-line: missing-fields
+            require('mason-lspconfig').setup {
+                automatic_enable = vim.tbl_keys(servers or {}),
+            }
             -- Ensure the servers and tools above are installed
             --
             -- To check the current status of installed tools and/or manually install
@@ -240,20 +244,12 @@ return {
             -- vim.list_extend(ensure_installed, {})
             require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-            require('mason-lspconfig').setup {
-                ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-                automatic_installation = false,
-                handlers = {
-                    function(server_name)
-                        local server = servers[server_name] or {}
-                        -- This handles overriding only values explicitly passed
-                        -- by the server configuration above. Useful when disabling
-                        -- certain features of an LSP (for example, turning off formatting for ts_ls)
-                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-                        require('lspconfig')[server_name].setup(server)
-                    end,
-                },
-            }
+            for server_name, config in pairs(servers) do
+                vim.lsp.config(server_name, config)
+            end
+
+            -- NOTE: Some servers may require an old setup until they are updated. For the full list refer here: https://github.com/neovim/nvim-lspconfig/issues/3705
+            -- These servers will have to be manually set up with require("lspconfig").server_name.setup{}
         end,
     },
 
