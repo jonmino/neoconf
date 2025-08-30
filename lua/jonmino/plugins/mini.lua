943b9bc941476bf4cb960c726d11caa409db9032
Jonas Minor <github@nc.seltsam.net>
2025-08-31 01:47:22 +0200

    Update to new mini.nvim location

--- lua/jonmino/plugins/mini.lua
+++ lua/jonmino/plugins/mini.lua
@@ -1,5 +1,5 @@
 return { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     config = function()
         -- Better Around/Inside textobjects
         --
@@ -32,6 +32,6 @@ return { -- Collection of various small independent plugins/modules
         end
 
         -- ... and there is more!
-        --  Check out: https://github.com/echasnovski/mini.nvim
+        --  Check out: https://github.com/nvim-mini/mini.nvim
     end,
 }
